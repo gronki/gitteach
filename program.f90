76d356af552ed7d2c29f1dae973389ada2e0ec9b
Dominik Gronkiewicz <gronki@gmail.com>
2026-04-20 22:15:05 +0200

    write 5 times

--- program.f90
+++ program.f90
@@ -1,6 +1,9 @@
 program testprogram
     implicit none
     character(len=64) :: name
+    integer :: i
     read (*, '(a)') name
+    do i = 1, 5
     print *, "Hello, ", trim(name), "!"
+    end do
 end program
